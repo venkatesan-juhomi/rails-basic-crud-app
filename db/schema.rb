89c15ebd2f43dedd46defd48397db055b74c57a0
venkatesan-Juhomi <venkatesans@juhomi.com>
2022-11-14 13:18:13 +0530

    Database changed from sqlilte3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_11_10_061723) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -29,7 +32,7 @@ ActiveRecord::Schema.define(version: 2022_11_10_061723) do
     t.string "name"
     t.integer "stars"
     t.string "comment"
-    t.integer "movie_id", null: false
+    t.bigint "movie_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
